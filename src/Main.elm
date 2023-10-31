1937683c907902b38b21c28009c5ff87faa291d2
ongaeshi <ongaeshi0621@gmail.com>
2023-11-01 00:59:38 +0900

    first commit

---
+++ src/Main.elm
@@ -0,0 +1,40 @@
+module Main exposing (main)
+
+import GraphicSVG exposing (..)
+import GraphicSVG.EllieApp exposing (GetKeyState,gameApp)
+
+init = { time = 0 }
+
+type Msg = Tick Float GetKeyState
+  
+update msg model =
+    case msg of
+        Tick t _ -> { model | time = t }
+
+myShapes model = [
+    triangle 40
+        |> filled darkRed
+        |> rotate (degrees 30)
+        |> scaleY 0.7
+    ,
+    circle 20
+        |> filled darkRed
+        |> move (17, 24)
+    ,
+    circle 20
+    |> filled darkRed
+    |> move (-17, 24)
+    ]
+
+view model =
+    collage 500 500 (myShapes model)
+
+main =
+    gameApp
+        Tick
+        { 
+            model = init
+        ,   view = view
+        ,   update = update
+        ,   title = "Ellie Example" -- This is the title of the browser window / tab
+        }
